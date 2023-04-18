ad473e8dea4235d647c676200bdaf4abd5b64604
qkrwnstlr <akfldkskgorn@gmail.com>
2023-04-18 14:19:33 +0900

    init project

---
+++ src/main/java/com/example/kumoh_school_bus/KumohSchoolBusApplication.java
@@ -0,0 +1,13 @@
+package com.example.kumoh_school_bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumohSchoolBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KumohSchoolBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kumoh_school_bus/KumohSchoolBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kumoh_school_bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KumohSchoolBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
